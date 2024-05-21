0aa50f835d8cd7267b1a43430a93a74f8d832c54
+ <+>
2024-05-20 22:54:01 -0400

    Finished Lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution in this file!
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    return {
+      ...obj,
+      [key]: value,
+    };
+  }
+  function destructivelyDeleteFromEmployeeByKey(obj, name,) {
+    delete employee[name];
+    return obj;
+  }
+  function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+    return obj;
+  };
+function deleteFromEmployeeByKey(obj, key, value) {
+    return {
+        ...obj,
+        [key]: value,
+    };
+}
+
+  const employee = {
+    name: "Courtney",
+    city: "Scottsdale",
+};
+const updatedEmployee = destructivelyUpdateEmployeeWithKeyAndValue(
+    employee = {
+        name: "Sam",
+        streetAddress: "12 Broadway",
+    }
+  );
+
